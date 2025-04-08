49606e5ed11bbcd6de186773202cd9307bccf403
Bogdan <sarapovbogdan04@gmail.com>
2025-04-08 13:43:48 +0300

    First commit

---
+++ REASite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace REASite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
