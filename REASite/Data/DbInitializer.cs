6d897056771bf74ce91aac0ea2a9c0fbf639f347
Bogdan <sarapovbogdan04@gmail.com>
2025-04-23 18:17:07 +0300

    added dbinitializer

---
+++ REASite/Data/DbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace REASite.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope()) 
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<REASiteDbContext>();
+                dbContext.Database.Migrate();
+            }
+
+        }
+    }
+}
