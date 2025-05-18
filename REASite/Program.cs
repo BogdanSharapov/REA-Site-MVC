0ad8e8017034f1004167f43d10c944a06b652935
Bogdan <sarapovbogdan04@gmail.com>
2025-05-18 19:34:09 +0300

    Identity and roles was added

--- REASite/Program.cs
+++ REASite/Program.cs
@@ -1,12 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using REASite.Data;
+using Microsoft.AspNetCore.Identity;
+using REASite.Areas.Identity.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireUppercase = false;
+});
 builder.Services.AddDbContext<REASiteDbContext>(options =>
-options.UseNpgsql(builder.Configuration.GetConnectionString("REASite")));
+options.UseNpgsql(builder.Configuration.GetConnectionString("REASiteDB")));
+
+builder.Services.AddDefaultIdentity<SiteUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<REASiteDbContext>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
@@ -41,5 +54,48 @@ DbInitializer.Initialize(app.Services);
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var roles = new List<string> { "Admin", "Manager", "User" };
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+            await roleManager.CreateAsync(new IdentityRole(role));
+
+    }
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<SiteUser>>();
+    string firstName = "Admin";
+    string lastName = "Admin";
+    string email = "admin@admin.com";
+    string password = "Admin123!@#";
+
+    if (await userManager.FindByEmailAsync(email) == null)
+    {
+        var user = new SiteUser();
+        user.FirstName = firstName;
+        user.LastName = lastName;
+        user.Email = email;
+        user.UserName = email;
+
+        var result = await userManager.CreateAsync(user, password);
+        if (result.Succeeded)
+        {
+            await userManager.AddToRoleAsync(user, "Admin");
+        }
+        else
+        {
+            foreach (var error in result.Errors)
+            {
+                Console.WriteLine(error.Description);
+            }
+        }
+    }
+}
 app.Run();
