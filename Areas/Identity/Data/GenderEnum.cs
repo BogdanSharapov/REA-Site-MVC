008607d2480096b69f800d33c65d0bc93ddb68fb
Bogdan <sarapovbogdan04@gmail.com>
2025-05-18 19:29:36 +0300

    Added Identity, User class and gender enum

---
+++ Areas/Identity/Data/GenderEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace REASite.Areas.Identity.Data
+{
+    public enum GenderEnum
+    {
+        NotSpecified = 0, // Значение по умолчанию: не указано
+        Male = 1,
+        Female = 2
+    }
+}
---
+++ Areas/Identity/Data/SiteUser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace REASite.Areas.Identity.Data;
+public class SiteUser : IdentityUser
+{
+    private string _firstName = string.Empty;
+    private string _lastName = string.Empty;
+
+    [PersonalData]
+    [Column(TypeName = "varchar(100)")]
+    public string FirstName
+    {
+        get => _firstName;
+        set
+        {
+            _firstName = value;
+            UpdateUserName();
+        }
+    }
+
+    [PersonalData]
+    [Column(TypeName = "varchar(100)")]
+    public string LastName
+    {
+        get => _lastName;
+        set
+        {
+            _lastName = value;
+            UpdateUserName();
+        }
+    }
+
+    public DateTime? DateOfBirth { get; set; }
+    public GenderEnum Gender { get; set; } = GenderEnum.NotSpecified;
+
+
+    public override string UserName
+    {
+        get => base.UserName;
+        set => base.UserName = value;
+    }
+
+    private void UpdateUserName()
+    {
+
+        base.UserName = $"{FirstName}{LastName}".Trim();
+    }
+}
