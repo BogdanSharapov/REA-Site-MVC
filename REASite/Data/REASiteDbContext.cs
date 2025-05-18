5eaa7168d87f0196c9787073a55af1538dd68b63
Bogdan <sarapovbogdan04@gmail.com>
2025-05-18 19:32:06 +0300

    modified

--- REASite/Data/REASiteDbContext.cs
+++ REASite/Data/REASiteDbContext.cs
@@ -1,35 +1,58 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using REASite.Areas.Identity.Data;
 using REASite.Models;
 using System.Configuration;
 
 namespace REASite.Data
 {
-    public class REASiteDbContext : DbContext
+    public class REASiteDbContext : IdentityDbContext<SiteUser>
     {
-        public DbSet<ApartmentModel> Apartments { get; set; } = null!;
-        public DbSet<Addres> Addres { get; set; } = null!;
+        public DbSet<Apartment> Apartments { get; set; } = null!;
+        public DbSet<Address> Addresses { get; set; } = null!;
+        public DbSet<ApartmentImage> ApartmentImages { get; set; } = null!;
 
         public REASiteDbContext(DbContextOptions<REASiteDbContext> options) : base(options)
         {
-       
+
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=REASitedb;Username=postgres;Password=4568");
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.UseIdentityColumns();
-            /*modelBuilder.Entity<ApartmentModel>().HasData(
-                new ApartmentModel { Id=1, Title = "Apartment1"},
-                new ApartmentModel { Id = 2, Title = "Apartment2" },
-                new ApartmentModel { Id = 3, Title = "Apartment3" },
-                new ApartmentModel { Id = 4, Title = "Apartment4" }
-                );*/
-            
-            
+            base.OnModelCreating(modelBuilder); //for identity
+
+            modelBuilder.Entity<Apartment>()
+                 .HasOne(a => a.Address)
+                 .WithMany(a => a.Apartments)
+                 .HasForeignKey(a => a.AddressId);
+
+            modelBuilder.Entity<ApartmentComfort>()
+                .HasKey(ac => new { ac.ApartmentId, ac.Comfort });
+
+            modelBuilder.Entity<ApartmentComfort>()
+                .HasOne(ac => ac.Apartment)
+                .WithMany(a => a.ApartmentComforts)
+                .HasForeignKey(ac => ac.ApartmentId);
+
+            modelBuilder.Entity<ApartmentImage>()
+                .HasOne(ai => ai.Apartment)
+                .WithMany(a => a.Images)
+                .HasForeignKey(ai => ai.ApartmentId);
+
+            // Добавляем уникальный индекс на поля адреса
+            modelBuilder.Entity<Address>()
+                .HasIndex(a => new { a.Country, a.City, a.Street, a.HouseNum })
+                .IsUnique();
+
+            //modelBuilder.Entity<SiteUser>()
+            //.Property(u => u.Gender)
+            //.HasConversion<string>()
+            //.HasColumnType("varchar(20)");
+
         }
     }
 
