8d2f717a057e908e7772efa8d1cfc559d5aca56c
Bogdan <sarapovbogdan04@gmail.com>
2025-05-18 19:31:50 +0300

    Added Identity, User class and gender enum

---
+++ REASite/Areas/Identity/Data/SiteUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace REASite.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the SiteUser class
+public class SiteUser : IdentityUser
+{
+    [PersonalData]
+    [Column(TypeName = "varchar(100)")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [PersonalData]
+    [Column(TypeName = "varchar(100)")]
+    public string LastName { get; set; } = string.Empty;
+
+    public DateTime? DateOfBirth { get; set; }
+    public GenderEnum Gender { get; set; } = GenderEnum.NotSpecified;
+}
+
